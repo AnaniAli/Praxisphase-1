e182f745c06ad37f066585e737cf85475a7ab8e2
Ali Anani <alianani@Mac.fritz.box>
2025-02-14 11:52:45 +0100

    Füge ngx-logger Ordner und Dateien hinzu

---
+++ ngx-logger/projects/ngx-logger/src/lib/models/log-publisher.ts
@@ -0,0 +1,11 @@
+// log-publisher.ts
+
+export class LogPublisher {
+    location: string;
+
+    constructor() {
+        this.location = ''; // Standardwert für location
+    }
+
+    // Du kannst hier zusätzliche Methoden oder Eigenschaften definieren, falls nötig.
+}
