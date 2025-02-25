95e1b7be67486ea8d42b9486026e0df278b50960
Ali Anani <alianani@Mac.fritz.box>
2025-02-25 12:17:51 +0100

    uebergabeDatenbank

--- aufgaben/backend-dotnet/obj/Debug/net9.0/backend-dotnet.AssemblyInfo.cs
+++ aufgaben/backend-dotnet/obj/Debug/net9.0/backend-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ace15e0dc8de667be8f8384c723c09153b9c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ff5ebdc6c49b8b57f831ddbca00e1730d53129")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
